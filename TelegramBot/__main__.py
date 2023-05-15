79cdf41a558dfbe9cc4ca85f294b3b693ee1550f
Mikey <125025968+Mikey-debug@users.noreply.github.com>
2023-05-15 19:26:34 +0530

    Update __main__.py

--- TelegramBot/__main__.py
+++ TelegramBot/__main__.py
@@ -6,13 +6,15 @@ if __name__ == "__main__":
     from pyrogram import types
     @bot.on_message(filters.command(["a", "b"]) & sudo_cmd)
         async def a():
-        chat_id = -1728546352
-        messages = await client.get_chat_history(chat_id, limit=1)
-        latest_message = messages[0] if messages else None
+            try:
+                chat_id = -1728546352
+                messages = await client.get_chat_history(chat_id, limit=1)
+                latest_message = messages[0] if messages else None
 
-        if latest_message:
-            await bot.reply_text("Latest message:", latest_message.text)
-        else:
-            print("No messages found in the group.")
-
+                if latest_message:
+                    await bot.reply_text("Latest message:", latest_message.text)
+                else:
+                    print("No messages found in the group.")
+            except Exception as e:
+                await bot.reply_text(e)
     bot.run()
