1e29a54f1e3275d6bfdb145d54cbc22a93322d95
Mikey <125025968+Mikey-debug@users.noreply.github.com>
2023-05-17 18:35:54 +0530

    Update __main__.py

--- TelegramBot/__main__.py
+++ TelegramBot/__main__.py
@@ -1,7 +1,7 @@
 from TelegramBot import bot
 from TelegramBot.logging import LOGGER
-
-from pyrogram import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram import Client
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 group = -1001728546352
 channel = -1001962326301
 # Create the callback game button
