743ea20a6140eada6408927024decfbf9596185a
Mikey <125025968+Mikey-debug@users.noreply.github.com>
2023-06-03 19:48:41 +0530

    Update __main__.py

--- TelegramBot/__main__.py
+++ TelegramBot/__main__.py
@@ -3,43 +3,6 @@ from TelegramBot.logging import LOGGER
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-group = -1001728546352
-channel = -1001962326301
-keyboard = None  # Declare the keyboard variable outside the function
-
-@bot.on_message(filters.command("test"))
-def test(client, message):
-    global keyboard  # Use the global keyword to access the keyboard variable
-    # Create the callback game button
-    callback_game_button = InlineKeyboardButton(
-        "Play Game",
-        callback_game="your_game_callback_data"
-    )
-
-    # Create an inline keyboard markup with the callback game button
-    keyboard = InlineKeyboardMarkup([[callback_game_button]])
-    post = bot.send_message(
-        chat_id=channel,
-        text="Hello! Please click the button below:",
-        reply_markup=keyboard
-    )
-
-@bot.on_callback_query()
-def handle_callback_game(client, callback_query):
-    global keyboard  # Use the global keyword to access the keyboard variable
-    # Get the callback game data
-    callback_game_data = callback_query.game_short_name
-    post = bot.send_message(
-        chat_id=channel,
-        text=callback_game_data,
-        reply_markup=keyboard
-    )
-    # Handle the game logic based on the callback data
-    # ...
-
-    # Answer the callback query to close the game
-    callback_query.answer()
-
 LOGGER(__name__).info("client successfully initiated....")
 if __name__ == "__main__":
     bot.run()
